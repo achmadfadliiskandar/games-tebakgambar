9736df05fb23e43c106c076f3d967db9cd718bb0
achmadfadliiskandar <af137357@gmail.com>
2022-09-19 21:04:21 +0700

    silahkan di coba sendiri

--- resources/views/pemain/start.blade.php
+++ resources/views/pemain/start.blade.php
@@ -12,8 +12,8 @@
                     <div class="card" style="width: 100%;">
                         {{-- <img src="{{asset('images/'.$game->images)}}" class="card-img-top" alt="gambar"> --}}
                         <div class="card-body" style="background: {{$game->warna}}">
-                        <h3 class="card-title">Judul : {{$game->judul}}</h3>
-                        <h5 class="card-subtitle mb-2 text-muted">Level : {{$game->level}}</h5>
+                        <h3 class="card-title">Soal : {{$game->level}}</h3>
+                        <h5 class="card-subtitle mb-2 text-muted">Judul : {{$game->judul}}</h5>
                         @if ($game->required == NULL)
                         <h6 class="card-text text-dark text-capitalize">tidak ada persyaratan/required</h6>
                         @else
